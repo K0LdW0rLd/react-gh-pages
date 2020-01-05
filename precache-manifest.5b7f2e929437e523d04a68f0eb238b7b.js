83bf4defd994447602313fa622a2bd028651c125
Kelly M Collins <90kcol@gmail.com>
2020-01-04 23:38:28 -0500

    Updates

---
+++ precache-manifest.5b7f2e929437e523d04a68f0eb238b7b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d866cdc4051f1aef309fbe16cacd5e97",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "2665991a0dad7232db90",
+    "url": "/me/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "54d19cd485d5cfe49523",
+    "url": "/me/static/css/main.4b6dd1e6.chunk.css"
+  },
+  {
+    "revision": "2665991a0dad7232db90",
+    "url": "/me/static/js/2.b9452cd6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/me/static/js/2.b9452cd6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54d19cd485d5cfe49523",
+    "url": "/me/static/js/main.0c291a02.chunk.js"
+  },
+  {
+    "revision": "789233a86dd94a98a9d5",
+    "url": "/me/static/js/runtime-main.2eae83fc.js"
+  },
+  {
+    "revision": "dd289d1cf1ff8b6613d7b09f47d86ed0",
+    "url": "/me/static/media/headshot.dd289d1c.jpg"
+  },
+  {
+    "revision": "e73dbe69576a77dd1ebd5fb0bcb229f3",
+    "url": "/me/static/media/multiColor.e73dbe69.jpg"
+  },
+  {
+    "revision": "a43c0e53cc0a0b8c83d3118731497f1e",
+    "url": "/me/static/media/playstationConsole.a43c0e53.jpg"
+  }
+]);
